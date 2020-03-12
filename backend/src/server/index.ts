c3f2e7d539537106dbafda318dc38aafad26c8cc
MishaNavrotsky <misha172xdd@gmail.com>
2020-03-12 19:27:12 +0200

    123

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -1,6 +1,5 @@
-import express, { request } from 'express'
+import express from 'express'
 import requests from './requests'
-import wsrequests from './wsrequests'
 import auth from "../authentication"
 import cors from "cors"
 import { initRoutes } from "./requests/routes"
@@ -11,7 +10,6 @@ import _ from "lodash"
 class server {
   app = express();
   requests: requests = null;
-  wsrequests: wsrequests = null;
   constructor() {
     this.app = express();
     this.app
@@ -23,7 +21,6 @@ class server {
       }));
     this.requests = new requests(this.app);
     initRoutes(this.requests)
-    // this.wsrequests = new wsrequests();
   }
 
   init(port) {
